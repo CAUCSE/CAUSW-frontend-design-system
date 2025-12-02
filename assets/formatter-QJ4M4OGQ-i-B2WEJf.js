c943e65fad63a94dc9bb2fb97a5cfb22c56745f2
kyh0726 <kyh0726@users.noreply.github.com>
2025-12-02 04:44:15 +0000

    deploy: 4023540c8734d3b68aa424b438ad68472188fa40

---
+++ assets/formatter-QJ4M4OGQ-i-B2WEJf.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-1ai0cinI.js";import{E as t}from"./iframe-fXGZ2J8l.js";import"./preload-helper-Dp1pzeXC.js";import"./react-18-BpohaTFu.js";import"./jsx-runtime-D6AYiBHE.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
