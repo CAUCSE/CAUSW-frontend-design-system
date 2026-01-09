58efb12540731dc68b3b11dd45ec8e9e16102f69
kyh0726 <younghyun75398@gmail.com>
2026-01-10 03:33:11 +0900

    docs: 문서 피그마 예시에 맞춰 변경

--- packages/core/src/components/Select/Select.stories.tsx
+++ packages/core/src/components/Select/Select.stories.tsx
@@ -17,15 +17,14 @@ export const Basic: Story = {
   render: () => (
     <Select>
       <Select.Trigger className="w-[300px]">
-        <Select.Value placeholder="Select a fruit" />
+        <Select.Value placeholder="--선택해주세요--" />
       </Select.Trigger>
       <Select.Content>
         <Select.Group>
-          <Select.Item value="apple">Apple</Select.Item>
-          <Select.Item value="banana">Banana</Select.Item>
-          <Select.Item value="blueberry">Blueberry</Select.Item>
-          <Select.Item value="grapes">Grapes</Select.Item>
-          <Select.Item value="pineapple">Pineapple</Select.Item>
+          <Select.Item value="apple">결혼</Select.Item>
+          <Select.Item value="banana">장례</Select.Item>
+          <Select.Item value="blueberry">졸업</Select.Item>
+          <Select.Item value="grapes">기타</Select.Item>
         </Select.Group>
       </Select.Content>
     </Select>
