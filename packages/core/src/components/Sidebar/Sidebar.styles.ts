db746ec1640f27f2a170f55173eb2fe46ecbd2a0
kyh0726 <younghyun75398@gmail.com>
2026-01-24 18:57:48 +0900

    styles: 피그마 맞춰서 스타일 수정

--- packages/core/src/components/Sidebar/Sidebar.styles.ts
+++ packages/core/src/components/Sidebar/Sidebar.styles.ts
@@ -1,25 +1,27 @@
-import { tv } from 'tailwind-variants';
+import { tv, VariantProps } from 'tailwind-variants';
 
 export const sidebarStyles = tv({
   slots: {
     root: 'flex h-screen w-65 flex-col bg-white border-r border-gray-100',
-    header: 'px-4.5 py-8',
-    content: 'flex-1 overflow-y-auto px-4.5 py-2',
+    header: 'px-2.5 py-8',
+    content: 'flex-1 overflow-y-auto px-2.5 py-2',
     footer: 'border-t border-gray-100 px-4 py-4',
-    item: 'group w-full flex items-center gap-2 px-2 py-2 rounded-sm text-md transition-colors',
+    item: 'group w-full flex items-center gap-2 px-4.5 py-2 rounded-sm text-md transition-colors',
     badge: 'ml-auto px-2 py-0.5 text-sm rounded-sm bg-red-100 text-red-400',
   },
   variants: {
-    active: {
+    selected: {
       true: {
-        item: 'bg-gray-100 text-gray-700 font-bold',
+        item: 'bg-gray-100 text-gray-700 typo-subtitle-16-bold',
       },
       false: {
-        item: 'text-gray-500 hover:bg-gray-50',
+        item: 'text-gray-500 hover:bg-gray-50 typo-body-16-medium',
       },
     },
   },
   defaultVariants: {
-    active: false,
+    selected: true,
   },
 });
+
+export type SidebarVariants = VariantProps<typeof sidebarStyles>;
