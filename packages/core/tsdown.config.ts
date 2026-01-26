65d4dec17560df3277d55765496ab446a4236fea
kyh0726 <younghyun75398@gmail.com>
2026-01-26 18:42:53 +0900

    feat: 업데이트 중단 이슈로 tsup -> tsdown 마이그레이션

---
+++ packages/core/tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  format: ['esm', 'cjs'],
+  clean: true,
+  dts: true,
+  unbundle: true,
+  loader: {
+    '.jpeg': 'dataurl',
+    '.png': 'dataurl',
+    '.svg': 'dataurl',
+  },
+});
