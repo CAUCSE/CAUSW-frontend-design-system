3b125739ef4cb0e6414e6a7073d5545fa9b097be
MinSeokKim <kimjunseok23@gmail.com>
2026-01-27 10:54:21 +0900

    chore: 바뀐 스크립트로 아이콘 재생성

--- packages/icons/src/mono/Comment.tsx
+++ packages/icons/src/mono/Comment.tsx
@@ -1,27 +1,36 @@
 import type { MonoIconProps } from '../types';
-import { DEFAULT_SIZE, MONO_COLORS } from '../types';
+import { DEFAULT_SIZE, MONO_COLORS, ICON_TOKEN_COLORS } from '../types';
 
 export const Comment = ({
   size = DEFAULT_SIZE,
   active = false,
+  color,
   title,
   ...props
-}: MonoIconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    width={size}
-    height={size}
-    fill={active ? MONO_COLORS.active : MONO_COLORS.inactive}
-    color={active ? MONO_COLORS.active : MONO_COLORS.inactive}
-    aria-hidden={title ? undefined : true}
-    aria-label={title}
-    role={title ? 'img' : undefined}
-    {...props}
-  >
-    {title && <title>{title}</title>}
-    <path d="M20 10C20 4.47714 15.5229 0 10 0C4.47714 0 0 4.47714 0 10C0 15.5229 4.47714 20 10 20C11.6914 20 13.2871 19.5793 14.6857 18.8364L18.6343 19.9593C18.8178 20.0113 19.0119 20.0134 19.1965 19.9653C19.381 19.9173 19.5495 19.8208 19.6843 19.6859C19.8191 19.551 19.9155 19.3825 19.9635 19.1979C20.0114 19.0133 20.0093 18.8192 19.9571 18.6357L18.8343 14.6879C19.6017 13.2446 20.0021 11.6346 20 10Z" />
-  </svg>
-);
+}: MonoIconProps) => {
+  const resolvedColor = color
+    ? ICON_TOKEN_COLORS[color]
+    : active
+      ? MONO_COLORS.active
+      : MONO_COLORS.inactive;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width={size}
+      height={size}
+      fill={resolvedColor}
+      color={resolvedColor}
+      aria-hidden={title ? undefined : true}
+      aria-label={title}
+      role={title ? 'img' : undefined}
+      {...props}
+    >
+      {title && <title>{title}</title>}
+      <path d="M20 10C20 4.47714 15.5229 0 10 0C4.47714 0 0 4.47714 0 10C0 15.5229 4.47714 20 10 20C11.6914 20 13.2871 19.5793 14.6857 18.8364L18.6343 19.9593C18.8178 20.0113 19.0119 20.0134 19.1965 19.9653C19.381 19.9173 19.5495 19.8208 19.6843 19.6859C19.8191 19.551 19.9155 19.3825 19.9635 19.1979C20.0114 19.0133 20.0093 18.8192 19.9571 18.6357L18.8343 14.6879C19.6017 13.2446 20.0021 11.6346 20 10Z" />
+    </svg>
+  );
+};
 
 Comment.displayName = 'Comment';
--- packages/icons/src/mono/Heart.tsx
+++ packages/icons/src/mono/Heart.tsx
@@ -1,27 +1,36 @@
 import type { MonoIconProps } from '../types';
-import { DEFAULT_SIZE, MONO_COLORS } from '../types';
+import { DEFAULT_SIZE, MONO_COLORS, ICON_TOKEN_COLORS } from '../types';
 
 export const Heart = ({
   size = DEFAULT_SIZE,
   active = false,
+  color,
   title,
   ...props
-}: MonoIconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    width={size}
-    height={size}
-    fill={active ? MONO_COLORS.active : MONO_COLORS.inactive}
-    color={active ? MONO_COLORS.active : MONO_COLORS.inactive}
-    aria-hidden={title ? undefined : true}
-    aria-label={title}
-    role={title ? 'img' : undefined}
-    {...props}
-  >
-    {title && <title>{title}</title>}
-    <path d="M8.98227 18.699C9.60056 19.1003 10.3989 19.1003 11.0172 18.699C12.9801 17.4249 17.2578 14.3817 19.0996 10.9753C21.5288 6.48177 18.6761 2 14.9064 2C12.7572 2 11.464 3.1029 10.7492 4.05089C10.3726 4.55043 9.62799 4.55043 9.25084 4.05089C8.53597 3.1029 7.24281 2 5.09363 2C1.32385 2 -1.52877 6.48177 0.900417 10.9753C2.74159 14.3811 7.01938 17.4249 8.98227 18.699Z" />
-  </svg>
-);
+}: MonoIconProps) => {
+  const resolvedColor = color
+    ? ICON_TOKEN_COLORS[color]
+    : active
+      ? MONO_COLORS.active
+      : MONO_COLORS.inactive;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      width={size}
+      height={size}
+      fill={resolvedColor}
+      color={resolvedColor}
+      aria-hidden={title ? undefined : true}
+      aria-label={title}
+      role={title ? 'img' : undefined}
+      {...props}
+    >
+      {title && <title>{title}</title>}
+      <path d="M8.98227 18.699C9.60056 19.1003 10.3989 19.1003 11.0172 18.699C12.9801 17.4249 17.2578 14.3817 19.0996 10.9753C21.5288 6.48177 18.6761 2 14.9064 2C12.7572 2 11.464 3.1029 10.7492 4.05089C10.3726 4.55043 9.62799 4.55043 9.25084 4.05089C8.53597 3.1029 7.24281 2 5.09363 2C1.32385 2 -1.52877 6.48177 0.900417 10.9753C2.74159 14.3811 7.01938 17.4249 8.98227 18.699Z" />
+    </svg>
+  );
+};
 
 Heart.displayName = 'Heart';
