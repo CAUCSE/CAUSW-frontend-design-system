f5e50ab2ffaec9a91d603f50b4b2a62a362b8157
kyh0726 <kyh0726@users.noreply.github.com>
2026-01-29 17:12:11 +0000

    deploy: 784d5e0dff0c2b2979021ecc3187b7179b53097b

---
+++ assets/formatter-QJ4M4OGQ-DDWA1qan.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-BLXR4nFd.js";import{I as t}from"./iframe-BwZachVF.js";import"./preload-helper-Dp1pzeXC.js";import"./react-18-l5_3Bgqn.js";import"./index-CX2T5C9G.js";import"./index-CAdTr0uw.js";import"./jsx-runtime-D_zvdyIk.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
