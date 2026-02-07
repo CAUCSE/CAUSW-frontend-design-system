b75c577376e6509169d6ae33de044dd8695ee8a7
kyh0726 <younghyun75398@gmail.com>
2026-02-08 06:24:42 +0900

    chore: 분류 변경

--- packages/core/src/components/Spacer/Spacer.stories.tsx
+++ packages/core/src/components/Spacer/Spacer.stories.tsx
@@ -6,7 +6,7 @@ import { Box } from '../Box';
 import { Flex } from '../Flex';
 
 const meta = {
-  title: 'Layout/Spacer',
+  title: 'Layouts/Spacer',
   component: Spacer,
   parameters: {
     layout: 'centered',
