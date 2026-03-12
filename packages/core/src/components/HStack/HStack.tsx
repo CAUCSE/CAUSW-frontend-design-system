42787e9aa472fda8ce2fb3b7ee9e80d153c3d29f
kyh0726 <younghyun75398@gmail.com>
2026-03-12 11:44:28 +0900

    fix: vstack, hstack에서 타입 제대로 처리되지 않는 이슈 핸들링

--- packages/core/src/components/HStack/HStack.tsx
+++ packages/core/src/components/HStack/HStack.tsx
@@ -1,15 +1,14 @@
 import React, { type ElementType } from 'react';
 import { Stack, type StackProps } from '../Stack';
 
-export type HStackProps<E extends ElementType = 'div'> = Omit<
-  StackProps<E>,
-  'direction'
->;
+export type HStackProps<E extends ElementType = 'div'> = StackProps<E> & {
+  direction?: never;
+};
 
 export const HStack = <E extends ElementType = 'div'>({
   ...props
 }: HStackProps<E>) => {
-  return <Stack<E> direction="horizontal" {...(props as StackProps<E>)} />;
+  return <Stack {...props} direction="horizontal" />;
 };
 
 HStack.displayName = 'HStack';
